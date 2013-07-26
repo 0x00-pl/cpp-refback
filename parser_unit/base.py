6abb2ea83d4314dacddabc466c44eb340bdbb417
0x00-pl <0x00.pl@gmail.com>
2013-07-26 15:57:03 +0800

    add stmt_include
    rename ln_* to stmt_*

--- parser_unit/base.py
+++ parser_unit/base.py
@@ -14,6 +14,14 @@ def pl_may(fn):
    return rf
  return ret
 
+def pl_not(fn):
+ def ret(text):
+  if fn(text)==None:
+   return (tuple(),text)
+  else:
+   return None
+ return ret
+
 def pl_until(fn):
  def ret(text):
   cut=0
@@ -82,6 +90,8 @@ def _prettyprint(rf):
  else:
   return (_link_str(rf[0]),rf[1])
 
+'''token'''
+
 def token_comment_c(text):
  cs='/*'
  ce='*/'
@@ -145,17 +155,39 @@ token_spacen= pl_or(token_space,token_newline)
 _EN_set= 'abcdefghijklmnopqrstuvwxyz'
 _symbol_char_start= '_'+_EN_set+_EN_set.upper()
 _symbol_num= '0123456789'
+_symbol_num_0x= _symbol_num+_EN_set[:6]+_EN_set[:6].upper()
 
-'symbol:[_a-zA-Z][_a-zA-Z0-9]*'
 _token_symbol_unprettyprint= pl_link(pl_any_char(_symbol_char_start),pl_mult(pl_any_char(_symbol_char_start+_symbol_num)))
-r'num:[0-9]*\.[0-9]*(e|E)[0-9]*'
-_token_num_unprettyprint= pl_link(pl_any_char(_symbol_num),pl_any_char('.'),pl_any_char(_symbol_num),pl_any_char('eE'),pl_any_char(_symbol_num))
+_token_symbol_unprettyprint.__doc__='symbol:[_a-zA-Z][_a-zA-Z0-9]*'
+
+_token_num_dec_unprettyprint= pl_link(pl_mult(pl_any_char(_symbol_num)),pl_any_char('.'),pl_mult(pl_any_char(_symbol_num)),pl_any_char('eE'),pl_mult(pl_any_char(_symbol_num)))
+_token_num_dec_unprettyprint.__doc__=r'num-dec:[0-9]*\.[0-9]*(e|E)[0-9]*'
+
+_token_num_hex_unprettyprint= pl_link(pl_const('0x'),pl_mult(pl_any_char(_symbol_num_0x)))
+_token_num_hex_unprettyprint.__doc__=r'num-hex:0x[0-9a-fA-F]*'
+
+_token_num_unprettyprint= pl_or(_token_num_dec_unprettyprint,_token_num_hex_unprettyprint)
+
 def token_symbol(text):
  return _prettyprint(_token_symbol_unprettyprint(text))
 
-ln_define= pl_link(pl_const('#define'),token_space,token_symbol,pl_may(pl_until(pl_any_char('\n'))),pl_any_char('\n'))
-'#define symbol( exp)? newline'
- 
+def token_num(text):
+ return _prettyprint(_token_num_unprettyprint(text))
+
+
+'''stmt'''
+
+_stmt_define_unprettyprint= pl_link(pl_const('#define'),token_space,token_symbol,pl_may(pl_until(pl_any_char('\n'))),pl_any_char('\n'))
+_stmt_define_unprettyprint.__doc__=r'#define symbol( exp)?\n'
+def stmt_define(text):
+ tmp= _stmt_define_unprettyprint(text)
+ return (('#define',tmp[0][2],token_space(' '+tmp[0][3])[1]),tmp[1])
+
+_stmt_include_unprettyprint= pl_link(pl_const('#include'),pl_until(token_newline),token_newline)
+_stmt_include_unprettyprint.__doc__=r'#include .* newline'
+def stmt_include(text):
+ tmp= _stmt_include_unprettyprint(text)
+ return (('#include',token_space(' '+tmp[0][1])[1]or''),tmp[1])
 
 if __name__=='__main__':
  '''test case'''
@@ -163,12 +195,16 @@ if __name__=='__main__':
  print('123%123',match_123('123'))
  match_123_or_456= pl_or(match_123,pl_const('456'))
  print('456%123|456',match_123_or_456('456'))
+ print('until',pl_until(pl_const('456'))('123456'))
+ print('n*',pl_mult(match_123)('123123123456'))
  print('comment',token_comment('/*123*/456'))
  print('comment',token_comment('//123\n456'))
  print('space',token_space(' //line1\n \t\t/*line2*/ 456'))
- print('n*',pl_mult(match_123)("123123123456"))
- print('symbol',token_symbol("_as012df=self"))
- print('until',pl_until(pl_const('456'))("123456"))
- print('#define',ln_define("#define D\n456"))
- print('#define',ln_define("#define A 20\n456"))
+ print('symbol',token_symbol('_as012df=self'))
+ print('number',token_num('12.34e56E456'))
+ print('number',token_num('0x123x456'))
+ print('#define',stmt_define('#define D\n456'))
+ print('#define',stmt_define('#define A 20\n456'))
+ print('#include',stmt_include('#include<iostream>/*TODO fix*/ //123\n456'))
+ print('#include',stmt_include('#include /*000*/"stdio.h"//123\n456'))
  

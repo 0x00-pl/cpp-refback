d13fdaf59969567a8de02c2b552cff7ab92f3a6b
0x00-pl <0x00.pl@gmail.com>
2013-07-28 17:00:43 +0800

    add lexpl()
    add pl_mult_until() for '111112'
    add lexpl() for '(a*(b+1))'

--- parser_unit/base.py
+++ parser_unit/base.py
@@ -32,6 +32,22 @@ def pl_until(fn):
   return None
  return ret
 
+def pl_mult_until(fmult,funtil):
+ def ret(text):
+  collect=[]
+  tmptxt=text
+  while True:
+   rfu= funtil(tmptxt)
+   if rfu!=None:
+    return (tuple(collect),tmptxt)
+   '''iter next'''
+   rfm= fmult(tmptxt)
+   if rfm==None: return None
+   collect.append(rfm[0])
+   tmptxt= rfm[1]
+  return None
+ return ret
+
 def pl_link(*fn):
  def ret(text):
   rfn=[]
@@ -160,19 +176,25 @@ _symbol_num_0x= _symbol_num+_EN_set[:6]+_EN_set[:6].upper()
 _token_symbol_unprettyprint= pl_link(pl_any_char(_symbol_char_start),pl_mult(pl_any_char(_symbol_char_start+_symbol_num)))
 _token_symbol_unprettyprint.__doc__='symbol:[_a-zA-Z][_a-zA-Z0-9]*'
 
-_token_num_dec_unprettyprint= pl_link(pl_mult(pl_any_char(_symbol_num)),pl_any_char('.'),pl_mult(pl_any_char(_symbol_num)),pl_any_char('eE'),pl_mult(pl_any_char(_symbol_num)))
-_token_num_dec_unprettyprint.__doc__=r'num-dec:[0-9]*\.[0-9]*(e|E)[0-9]*'
+_token_num_dec_dot= pl_link(pl_any_char('.'),pl_mult(pl_any_char(_symbol_num)))
+_token_num_dec_e= pl_link(pl_any_char('eE'),pl_mult(pl_any_char(_symbol_num)))
+_token_num_dec_unprettyprint= pl_link(pl_any_char(_symbol_num),pl_mult(pl_any_char(_symbol_num)),pl_may(_token_num_dec_dot),pl_may(_token_num_dec_e))
+_token_num_dec_unprettyprint.__doc__=r'num-dec:[0-9]*(\.[0-9]*)?(e|E)[0-9]*'
 
 _token_num_hex_unprettyprint= pl_link(pl_const('0x'),pl_mult(pl_any_char(_symbol_num_0x)))
 _token_num_hex_unprettyprint.__doc__=r'num-hex:0x[0-9a-fA-F]*'
 
-_token_num_unprettyprint= pl_or(_token_num_dec_unprettyprint,_token_num_hex_unprettyprint)
+_token_num_unprettyprint= pl_or(_token_num_hex_unprettyprint,_token_num_dec_unprettyprint)
 
 def token_symbol(text):
  return _prettyprint(_token_symbol_unprettyprint(text))
 
 def token_num(text):
  return _prettyprint(_token_num_unprettyprint(text))
+
+token_op= pl_any_char('()[]{}<>=+-*/%!~^&|?:,')
+
+token= pl_or(token_op,pl_or(token_num,token_symbol))
 
 
 '''stmt'''
@@ -195,6 +217,37 @@ def stmt_usingnamespace(text):
  tmp= _stmt_usingnamespace(text)
  return tmp and (('using namespace',token_space(' '+tmp[0][1])[1]),tmp[1])
 
+def lexpl(text):
+ if not text.startswith('('): return None
+ collect=[]
+ iter=text[1:]
+ while True:
+  rfmu= pl_mult_until(token,pl_any_char('();'))(iter)
+  if rfmu==None: return None
+  collect+=rfmu[0]
+  iter=rfmu[1]
+  if iter.startswith(';'): return None
+  elif iter.startswith('('):
+   tmp= lexpl(iter)
+   if tmp==None:
+    return None
+   collect.append(tmp[0])
+   iter=tmp[1]
+  elif iter.startswith(')'):
+   return (tuple(collect),iter[1:])
+  else: print('ERROR: you may never go here')
+
+'''TODO
+def bloc(text):
+ if not text.startswith('{'): return None
+ tmp= pl_mult(stmt)(text[1:])
+ if not text.startswith('}'): return None
+ return tmp and (tmp[0],tmp[1][1:])
+
+def stmt_if(text):
+ if not text.startswith('if'): return None
+ tmp= pl_link(token_spacen)
+'''
 if __name__=='__main__':
  '''test case'''
  match_123= pl_const('123')
@@ -203,15 +256,19 @@ if __name__=='__main__':
  print('456%123|456',match_123_or_456('456'))
  print('until',pl_until(pl_const('456'))('123456'))
  print('n*',pl_mult(match_123)('123123123456'))
+ print('mult-until',pl_mult_until(pl_any_char('123'),pl_const('456'))('123321456'))
  print('comment',token_comment('/*123*/456'))
  print('comment',token_comment('//123\n456'))
  print('space*',pl_mult(token_spacen)(' //line1\n \t\t/*line2*/ 456'))
  print('symbol',token_symbol('_as012df=self'))
  print('number',token_num('12.34e56E456'))
  print('number',token_num('0x123x456'))
+ print('number',token_num('123-456'))
  print('#define',stmt_define('#define D\n456'))
  print('#define',stmt_define('#define A 20\n456'))
  print('#include',stmt_include('#include<iostream>/*TODO fix*/ //123\n456'))
  print('#include',stmt_include('#include /*000*/"stdio.h"//123\n456'))
  print('using-namespace',stmt_usingnamespace('using namespace std;\n456'))
+ print('token',pl_mult(token)('(a+b*(1e3+(1-2))%c/d^(0.5*e))456'))
+ print('(exp)',lexpl('(a+b*(1e3+(1-2))%c/d^(0.5*e))456'))
  
